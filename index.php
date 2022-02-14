36b655019b42587d3a9aa37537f7a87b5379f3b7
91Shapal <93367065+91Shapal@users.noreply.github.com>
2022-02-14 12:10:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+    <title>Altybaev Motors</title>
+    <link rel="icon" type="image/x-icon" href="https://us.123rf.com/450wm/kchung/kchung1503/kchung150301549/38110778-3d-shiny-diamond-letter-s-isolated-on-black-background.jpg?ver=6">
+</head>
+  
+<body>
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <nav class="nav">
+                <a class="nav__link active" href="#">About</a>
+                <a class="nav__link" href="#">Service</a>
+                <a class="nav__link" href="#">Bishkek</a>
+                <a class="nav__link" href="#">Moskovskaya 28</a>
+                <a class="nav__link" href="#">Contact: 0557481010</a>
+            </nav>
+        </div>
+    </div>
+</header>
 
+            </nav>
+        </div>
+    </div>
+<div class="intro">
+    <div class="container">
+        <div class="intro__inner">
+            <h2 class="intro__suptitle">Welcome To</h2>
+            <h1 class="intro__title">Altybaev Motors</h1>
+        </div>
+    </div>
+    <div class="slider">
+        <div class="container">
+            <div class="slider__inner">
+                <div class="slider__item active">
+                    <span class="slider__num">01</span>
+                    Intro
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">02</span>
+                    Work
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">03</span>
+                    About
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">04</span>
+                    Contacts
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
